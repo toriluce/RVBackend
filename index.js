a8104fe7231971a4b4d3d5401dd58ef5883db957
Tori <victorialuce34@gmail.com>
2022-10-12 10:48:12 -0500

    Address cors issues

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv').config()
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({ origin: true, credentials: true }));
+
 app.get("/ping", function(req, res) {
     res.json({
-        message: "ping works"});
+        message: "Ping works!"});
   });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-app.listen(port);
+app.listen(port, () => console.log(`Server is running on port ${port}`));
