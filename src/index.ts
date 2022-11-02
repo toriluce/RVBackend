d1600147770411ae511015ee24c6548b6adab119
Tori Luce <96202315+toriluce@users.noreply.github.com>
2022-11-02 16:05:51 -0500

    Add get route for a requested campgroundId and putReservedDate function to send ReservedDates to ddb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import express from 'express';
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
-import { createCampgroundHandler, sendPingHandler, getAllCampgroundsHandler } from './handlers';
+import {
+  createCampgroundHandler,
+  createReservedDateHandler,
+  getAllCampgroundsHandler,
+  getRequestedCampgroundHandler,
+} from "./handlers";
 
 const app = express();
 
@@ -10,12 +15,17 @@ app.use(express.json());
 
 app.use(cors({ origin: true, credentials: true }));
 
-app.get("/ping", sendPingHandler);
-
 app.get("/campgrounds", getAllCampgroundsHandler);
+
+app.get("/:campgroundId", getRequestedCampgroundHandler);
 
 app.post("/admin/campground", createCampgroundHandler);
 
+app.post("/admin/reservedDate", createReservedDateHandler);
+
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server is running on port ${port}`)
+);
+
